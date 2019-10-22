4004aa06ef82874ddcf13dc259bc9891a3ff1a16
ZY1N <zhang.yine@husky.neu.edu>
2019-10-22 14:24:53 -0700

    bbb

--- 01/phonebook.cpp
+++ 01/phonebook.cpp
@@ -16,23 +16,7 @@
 #include <climits>
 #include <cstring>
 #include <string.h>
-
-class contact
-{
-    public:
-        std::string firstName;
-        std::string lastName;
-        std::string nickName;
-        std::string login;
-        std::string postal;
-        std::string address;
-        std::string emailAddress;
-        std::string phoneNumber;
-        std::string birthdayDate;
-        std::string favoriteMeal;
-        std::string underwearColor;
-        std::string darkestSecret;
-};
+#include "phonebook.hpp"
 
 void    addFunction(contact *oneContact)
 {
